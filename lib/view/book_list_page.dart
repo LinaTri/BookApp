b073f5b7d75c9dee4f30c055a93771a0ffbb7b14
lina tri maulida <linamaulida894@gmail.com>
2022-12-17 00:25:27 +0700

    Delete

---
+++ lib/view/book_list_page.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: unused_import, prefer_typing_uninitialized_variables
+
+import 'dart:convert';
+
+import 'package:book_app/models/book_list_response.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+import '../controllers/book_controller.dart';
+import 'detail_book_page.dart';
+
+class BookListPage extends StatefulWidget {
+  const BookListPage({Key? key}) : super(key: key);
+
+  @override
+  State<BookListPage> createState() => _BookListPageState();
+}
+
+class _BookListPageState extends State<BookListPage> {
+  BookController? bookController;
+  bool isSearching = false;
+
+  @override
+  void initState() {
+    super.initState();
+    bookController = Provider.of<BookController>(context, listen: false);
+    bookController!.fetchBookApi();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var bookcontroller;
+    var consumer = Consumer(
+      child: const Center(child: CircularProgressIndicator()),
+      builder: (context, controlller, child) => Container(
+        child: bookcontroller!.bookList == null
+            ? child
+            : ListView.builder(
+                itemCount: bookController!.bookList!.books!.length,
+                itemBuilder: (context, index) {
+                  final currentBook = bookController!.bookList!.books![index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => DetailBookPage(
+                            isbn: currentBook.isbn13!,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      children: [
+                        Image.network(
+                          currentBook.image!,
+                          height: 100,
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(currentBook.title!),
+                                Text(currentBook.subtitle!),
+                                Align(
+                                    alignment: Alignment.topRight,
+                                    child: Text(currentBook.price!)),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Book Catalogue"),
+      ),
+      body: consumer,
+    );
+  }
+}
